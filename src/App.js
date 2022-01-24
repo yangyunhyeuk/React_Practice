0d7fc526bd26afbcbe7e22d761943f066ce49820
azz4622@naver.com <azz4622@naver.com>
2022-01-24 16:17:53 +0900

    Write_getLists_work

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-// useRef (디자인)
-// dom을 변경할 때 사용
-
-import { Route } from 'react-router-dom';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
+import ListPage from './pages/ListPage';
 
 function App() {
   return (
     <div>
-      <Header />
-      <Route path="/" exact={true} component={HomePage} />
-      <Route path="/login" exact={true} component={LoginPage} />
-      <Footer />
+      <ListPage />
     </div>
   );
 }
